1fe5911dd17cc1a6a3537c3ea562f62e788d35b8
Jan Funke <funkej@janelia.hhmi.org>
2023-03-11 15:59:26 -0500

    Add mask support to HammingCosts

--- structsvm/hamming_costs.py
+++ structsvm/hamming_costs.py
@@ -4,7 +4,7 @@ from .linear_costs import LinearCosts
 
 class HammingCosts(LinearCosts):
 
-    def __init__(self, ground_truth):
+    def __init__(self, ground_truth, mask=None):
 
         # H(y',y) = Σ|y'_i - y_i|
         #         = Σ_{i:y'_i = 1} (1-y_i) + Σ_{i:y'_i = 0} y_i
@@ -16,6 +16,10 @@ class HammingCosts(LinearCosts):
 
         coefficients = np.ones_like(ground_truth, dtype=np.float32)
         coefficients -= 2*ground_truth
+
+        if mask is not None:
+            ground_truth = ground_truth * mask
+            coefficients *= mask
         offset = np.sum(ground_truth)
 
         self.set_coefficients(coefficients)
