bf4f4f3fb2d3741e77b55336a9310321e1f62be7
Talley Lambert <talley.lambert@gmail.com>
2023-05-02 20:38:44 -0400

    bump version 0.1.1

--- structsvm/__init__.py
+++ structsvm/__init__.py
@@ -2,5 +2,5 @@ from .bundle_method import BundleMethod
 from .soft_margin_loss import SoftMarginLoss
 from .hamming_costs import HammingCosts
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __all__ = ['BundleMethod', 'SoftMarginLoss', 'HammingCosts']
